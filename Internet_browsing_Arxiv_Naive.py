29faa9093de20f7beecbc650ea02e2479a09de0b
Jayakrishna20 <jayakrishnans2041@gmail.com>
2024-02-22 20:41:24 +0530

    AI researcher and web browsing agents done

---
+++ Internet_browsing_Arxiv_Naive.py
@@ -0,0 +1,25 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import load_tools, initialize_agent, AgentType
+import os
+
+os.environ['OPENAI_API_KEY'] = 'OPENAI_API_KEY'
+
+# If the parser is erroring out, remember to set temperature to a higher value!!!
+
+llm = ChatOpenAI(temperature=0.3)
+tools = load_tools(
+    ["arxiv"]
+)
+
+agent_chain = initialize_agent(
+    tools,
+    llm,
+    max_iterations=5,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True,
+    handle_parsing_errors=True,  # IMPORTANT
+)
+
+agent_chain.run(
+    "what is RLHF?",
+)
